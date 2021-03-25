ad138c47a6426542244a37276e2703228963be8a
Rahul Patni <rahul20patni@gmail.com>
2021-03-24 23:55:37 -0700

    adding stack navigator

--- App.js
+++ App.js
@@ -1,27 +1,91 @@
 import { Provider as BlogProvider } from "./src/context/BlogContext";
 import React from "react";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+// import { createAppContainer } from "react-navigation";
+// import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, Text, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const navigator = createStackNavigator(
-  {
-    Index: IndexScreen,
-  },
-  {
-    initialRouteName: "Index",
-    defaultNavigationOptions: {
-      title: "Blogs",
-    },
-  }
-);
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-const App = createAppContainer(navigator);
+// const navigator = createStackNavigator(
+//   {
+//     Index: IndexScreen,
+//   },
+//   {
+//     initialRouteName: "Index",
+//     defaultNavigationOptions: {
+//       title: "Blogs",
+//     },
+//   }
+// );
+
+// const App = createAppContainer(navigator);
+
+const SettingsStack = createStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default () => {
   return (
     <BlogProvider>
-      <App />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Home") {
+                iconName = focused
+                  ? "ios-information-circle"
+                  : "ios-information-circle-outline";
+              } else if (route.name === "Settings") {
+                iconName = focused ? "ios-list-box" : "ios-list";
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+          }}
+        >
+          <Tab.Screen name="Home" component={IndexScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </BlogProvider>
   );
 };
