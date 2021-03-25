220654ccdf251ea4a5f06a39b035b0c7d87749f3
Rahul Patni <rahul20patni@gmail.com>
2021-03-24 23:58:45 -0700

    ADding ignore

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 
 export default (reducer, actions, initialState) => {
+  // @ts-ignore
   const Context = React.createContext();
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
